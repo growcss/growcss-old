d57ebea6d9ed3d0c12d19537bd015057ceb60ab7
anolilab <info@anolilab.de>
2015-06-22 17:38:02 +0200

    fix jasmin, added link to config repo

--- tasks/jasmine.js
+++ tasks/jasmine.js
@@ -2,24 +2,26 @@
 //jasmine config
 
 module.exports = {
-jasmine: {
-    test: {
-        src: ['src/js/**/ui.*.js'],
-        options: {
-            outfile: 'tests/_specRunner.html',
-            keepRunner: true,
-            specs: [
-                'tests/specs/setup.spec.js',
-                'tests/src/scss/*Tests.js',
-                'tests/specs/ui.*.spec.js',
-            ],
-            helpers: [
-                'bower_components/jasmine-jquery/lib/jasmine-jquery.js'
-            ],
-            styles: [
-                'build/css/growcss.css'
-            ]
-        }
+test: {
+    src: ['src/js/**/ui.*.js'],
+    options: {
+        outfile: 'tests/_specRunner.html',
+        keepRunner: true,
+        specs: [
+            'tests/specs/setup.spec.js',
+            'tests/src/scss/*Tests.js',
+            'tests/specs/ui.*.spec.js',
+        ],
+        helpers: [
+            'bower_components/jasmine-jquery/lib/jasmine-jquery.js'
+        ],
+        styles: [
+            'build/css/growcss.css'
+        ],
+        vendor: [
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/fastclick/lib/fastclick.js'
+        ]
     }
- }
+}
 };
