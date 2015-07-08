8e79ea11b84e6a47214ff1c38430fd7f85b25ebb
Daniel Bannert <d.bannert@anolilab.de>
2015-07-08 23:29:35 +0200

    added pixrem, added full support for typo remove old variable remove old function added @charset "UTF-8"; to all files small changes on well and menu

--- tasks/postcss.js
+++ tasks/postcss.js
@@ -1,14 +1,13 @@
 'use strict';
 //postcss config
 
-var autoprefixer = require('autoprefixer-core');
-
 module.exports = {
     options: {
       processors: [
-        autoprefixer({
+        require('pixrem')(),
+        require('autoprefixer-core')({
             browers: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
-        }).postcss
+        })
       ]
     },
     dist: {
