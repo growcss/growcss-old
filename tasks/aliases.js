201738d256dd8732a822206ce502814fe3d75143
daniel bannert <d.bannert@anolilab.de>
2016-02-16 21:56:33 +0100

    remove build from ignore | remove shrinkwrap

--- tasks/aliases.js
+++ tasks/aliases.js
@@ -14,10 +14,8 @@ module.exports = {
         'cs-test',
         'bump:master'
     ],
-    // Task for updating the cached npm packages used by the Travis build (which are controlled by test-infra/npm-shrinkwrap.json).
     // This task should be run and the updated file should be committed whenever GrowCss's dependencies change.
     'update': [
-      'exec:npmUpdate',
-      'shrinkwrap'
+      'exec:npmUpdate'
     ]
 };
