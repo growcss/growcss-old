1ab5f1320af987080dd0fce100f4b31509b23d2b
daniel bannert <d.bannert@anolilab.de>
2016-02-17 01:28:47 +0100

    init

--- build/component/create-component.sh
+++ build/component/create-component.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env php
 <?php
 
-$sourceDirectory = __DIR__.'/../src/components';
-$readmeTemplate  = __DIR__.'/README.md';
+$sourceDirectory   = __DIR__.'/../src/components';
+$readmeTemplate    = __DIR__.'/README.md';
+$indexTemplate     = __DIR__.'/index.html';
+$gitignoreTemplate = __DIR__.'/.gitignore';
+$bowerTemplate     = __DIR__.'/bower.json';
 
 $dirs = glob($sourceDirectory.'/*', GLOB_ONLYDIR);
 
@@ -11,9 +14,18 @@ foreach ($dirs as $dir) {
     $parts = explode('/', $dir);
     // set variables
     list($vendor, $name) = array_slice($parts, - 2);
-    $readme  = $sourceDirectory.'/'.$name.'/README.md';
+
+    $componentsDir = $dir.'/'.$name;
+    $readme  = $componentsDir.'/README.md';
 
     if (!file_exists($readme)) {
+
+        // create folder
+        mkdir($componentsDir.'/demo');
+        mkdir($componentsDir.'/test');
+        mkdir($componentsDir.'/css');
+        mkdir($componentsDir.'/js');
+
         $package = strtolower($vendor.'/'.$name);
         // get template
         $content = file_get_contents($readmeTemplate);
@@ -28,6 +40,10 @@ foreach ($dirs as $dir) {
 
         // write package readme
         file_put_contents($readme, $output);
+        ile_put_contents($bowerTemplate, $output);
+
+        // copy needed files
+        copy($file, $componentsDir.'/index.html');
 
         echo "Created README.md in {$package}</br>\r\n";
     }
