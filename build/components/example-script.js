11afc766f7a6da98e016a490d5a19e18fc3f58e1
daniel bannert <d.bannert@anolilab.de>
2016-03-08 23:38:33 +0100

    update example-script and example-test-index

--- build/components/example-script.js
+++ build/components/example-script.js
@@ -1,22 +1,22 @@
-(function () {
-  'use strict';
+'use strict';
 
-  class Class {
-    // Element setup goes here instead of created() callback
-    beforeRegister () {
-      this.is = '@name';
-      this.properties = {};
-    }
-
-    // Define other lifecycle methods as you need
-    registered () {}
-    created () {}
-    ready () {}
-    factoryImpl () {}
-    attached () {}
-    detached () {}
-    attributChanged () {}
+class Class {
+  // Element setup goes here instead of created() callback
+  beforeRegister() {
+    this.is = '@name';
+    this.properties = {};
   }
 
-  Polymer(Class);
-})();
+  // Define other lifecycle methods as you need
+  registered() {}
+  created() {}
+  ready() {}
+  factoryImpl() {}
+  attached() {}
+  detached() {}
+  attributChanged() {}
+}
+
+/*eslint-disable */
+Polymer(Class);
+/*eslint-enable */
