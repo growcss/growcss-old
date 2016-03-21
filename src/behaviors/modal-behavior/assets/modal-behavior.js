b3a4fb9f8d54e89a68dae310da953a40c3baacc2
daniel bannert <d.bannert@anolilab.de>
2016-03-21 22:43:56 +0100

    start working on behavior

--- src/behaviors/modal-behavior/assets/modal-behavior.js
+++ src/behaviors/modal-behavior/assets/modal-behavior.js
@@ -4,8 +4,48 @@
 const GrowCss = GrowCss || {};
 /*eslint-enable */
 
-const GcClass = {
-  // Behavior definition
+const GcModal = {
+  keyBindings: {
+    esc: 'close',
+  },
+
+  /**
+   * closes the modal
+   */
+  close() {
+    this._shown = false;
+  },
+
+  /**
+   * opens the modal
+   */
+  show() {
+    this._shown = true;
+  },
+
+  /**
+   * opens or closes the modal
+   */
+  toggle() {
+    this._shown = !this._shown;
+  },
+
+  /**
+   * change handler which will either call show or close based on the shown value.
+   */
+  // _shownHandler(newVal, oldVal) {
+  //   const func = newVal ? show : close;
+
+  //   if (oldVal === undefined) {
+  //     return;
+  //   }
+
+  //   func.call(this);
+  // },
+
+  attached() {
+
+  },
 };
 
-GrowCss.GcClass = GcClass;
+GrowCss.ModalBehavior = [Polymer.IronA11yKeysBehavior, GcModal];
